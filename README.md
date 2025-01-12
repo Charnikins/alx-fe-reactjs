124575e6104174322a98c479f863beec0e0672d4
alx-fe-reactjs <charnie.m@live.co.za>
2025-01-12 13:38:52 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+touch README.md
+git add .
+git commit -m "Set up React app with Vite"
+git push origin main
---
+++ app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to my React App!</h1>
+      <p>This app was created with Vite and React.</p>
+    </div>
+  );
+}
+
+export default App;
